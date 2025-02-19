6680857ee9f3ae39279cdf1b7dea386a61bcf77a
ruattt <shuxd01@163.com>
2025-02-19 22:21:29 +0800

    modified:   app/checkout/biz/service/checkout.go 	modified:   app/checkout/go.mod 	modified:   app/checkout/go.sum 	new file:   app/checkout/infra/mq/nats.go 	modified:   app/frontend/biz/service/order_list.go 	modified:   app/frontend/main.go 	modified:   app/frontend/utils/errors.go 	new file:   demo/front/.gitignore 	new file:   demo/front/.hz 	new file:   demo/front/biz/dal/init.go 	new file:   demo/front/biz/dal/mysql/init.go 	new file:   demo/front/biz/dal/redis/init.go 	new file:   demo/front/biz/handler/home/home_service.go 	new file:   demo/front/biz/handler/home/home_service_test.go 	new file:   demo/front/biz/router/home/home.go 	new file:   demo/front/biz/router/home/middleware.go 	new file:   demo/front/biz/router/register.go 	new file:   demo/front/biz/service/home.go 	new file:   demo/front/biz/utils/resp.go 	new file:   demo/front/build.sh 	new file:   demo/front/conf/conf.go 	new file:   demo/front/conf/dev/conf.yaml 	new file:   demo/front/conf/online/conf.yaml 	new file:   demo/front/conf/test/conf.yaml 	new file:   demo/front/docker-compose.yaml 	new file:   demo/front/go.mod 	new file:   demo/front/go.sum 	new file:   demo/front/hertz_gen/api/api.pb.go 	new file:   demo/front/hertz_gen/frontend/common/common.pb.go 	new file:   demo/front/hertz_gen/frontend/home/home.pb.go 	new file:   demo/front/main.go 	new file:   demo/front/readme.md 	new file:   demo/front/script/bootstrap.sh 	new file:   demo/front/static/css/bootstrap.min.css 	new file:   demo/front/static/image/logo.jpg 	new file:   demo/front/static/image/mouse-pad.jpeg 	new file:   demo/front/static/image/notebook.jpeg 	new file:   demo/front/static/image/sweatshirt.jpeg 	new file:   demo/front/static/image/t-shirt-1.jpeg 	new file:   demo/front/static/image/t-shirt-2.jpeg 	new file:   demo/front/static/image/t-shirt.jpeg 	new file:   demo/front/static/image/toiletry-bag.jpg 	new file:   demo/front/static/image/toothbrush-cup.jpg 	new file:   demo/front/static/js/bootstrap.bundle.min.js 	new file:   demo/front/template/footer.tmpl 	new file:   demo/front/template/header.tmpl 	new file:   demo/front/template/home.tmpl

---
+++ demo/front/biz/handler/home/home_service.go
@@ -0,0 +1,31 @@
+package home
+
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"gomall_study/demo/front/biz/service"
+	"gomall_study/demo/front/biz/utils"
+	common "gomall_study/demo/front/hertz_gen/frontend/common"
+)
+
+// Home .
+// @router / [GET]
+func Home(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req common.Empty
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	resp, err := service.NewHomeService(ctx, c).Run(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	c.HTML(consts.StatusOK, "home.tmpl", resp)
+}
